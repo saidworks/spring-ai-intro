27673ab8132fa29f997fb77e7a1d4171c4f5e9ab
Said Zitouni <zitouni.sd@gmail.com>
2025-02-26 21:48:30 -0500

    modify for azure and add config with exercise for templates

--- src/main/java/guru/springframework/springaiintro/model/GetCapitalResponse.java
+++ src/main/java/guru/springframework/springaiintro/model/GetCapitalResponse.java
@@ -2,5 +2,8 @@ package guru.springframework.springaiintro.model;
 
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
-public record GetCapitalResponse(@JsonPropertyDescription("This is the city name") String answer) {
+public record GetCapitalResponse(@JsonPropertyDescription("This is the city name") String answer,
+                                 @JsonPropertyDescription("This is the city population") String population,
+                                 @JsonPropertyDescription("This is the city's region") String region,
+                                 @JsonPropertyDescription("This is the currency used in the city") String currency) {
 }
